1b2f227be60aee06dbb5cfae09de25ab037272d1
User Zn3sky <zn3sky@www1896.sakura.ne.jp>
2017-03-07 02:27:02 +0900

    update

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/project-homepage/parts/common_header.html");
+?>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+
+<div class="container">
+<h1>Hello, world!</h1>
+<p>個人の学習用サイトです。コードはGitHubの<a href="https://github.com/zn3sky" target="_blank">@zn3sky</a>にあります。</p>
+<!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>-->
+</div>
+</div>
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-4">
+<h2>PHP Hello,world</h2>
+<p>PHP　勉強用のページです。PHP Version 7.1.1で動作確認済みです。 </p>
+<p><a class="btn btn-default" href="/test/sample-php-v7/" role="button">View details &raquo;</a></p>
+</div>
+
+<div class="col-md-4">
+<h2>JavaScript</h2>
+<p>JavaScriptサンプル集です。</p>
+<p><a class="btn btn-default" href="/sample/javascript/" role="button">View details &raquo;</a></p>
+</div>
+
+<div class="col-md-4">
+<h2>amCharts</h2>
+<p>amChartsというグラフを簡単に描けるライブラリを使ったdemoです。 javascript版V3を利用しています。</p>
+<p><a class="btn btn-default" href="/test/sample-javascript-amcharts-v3/" role="button">View details &raquo;</a></p>
+</div>
+
+<div class="col-md-4">
+<h2>PHP - Laravel</h2>
+<p>Laravel5の勉強用ページです。 </p>
+<p><a class="btn btn-default" href="/test/test_laravel/test/public/" role="button">View details &raquo;</a></p>
+</div>
+
+</div>
+
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/project-homepage/parts/common_footer.html");
+?>
